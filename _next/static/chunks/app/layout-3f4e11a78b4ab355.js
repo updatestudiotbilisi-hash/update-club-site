10f6f77b4ecfce3dd817fda05c37bb47a9e7bffb
 Svetlana Demkina <svetlanademkina@Sveta.local>
2026-09-04 16:30:03 +0400

    Publish UPDATE redesign as static GitHub Pages homepage

---
+++ _next/static/chunks/app/layout-3f4e11a78b4ab355.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3793:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6872,23)),Promise.resolve().then(n.t.bind(n,7686,23))},6872:()=>{},7686:()=>{}},e=>{e.O(0,[18,127,441,794,358],()=>e(e.s=3793)),_N_E=e.O()}]);
